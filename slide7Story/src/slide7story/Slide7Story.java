26acf9e657814b15a5a0838e3f5331f4e4d87552
Ioannis <moutzouris@aegean.gr>
2020-11-02 13:13:18 +0200

    test gitignore

---
+++ slide7Story/src/slide7story/Slide7Story.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slide7story;
+
+/**
+ *
+ * @author user
+ */
+public class Slide7Story {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String answer = "";
+        for(int i = 0; i < 10; i++){
+            answer += "*";
+        }
+        System.out.println(answer);
+    }
+    
+}
